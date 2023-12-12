fecc236db63acdbb0cfb9b1bed94e907df0ed159
Ben Fidlers <ben.fidlers@qmino.com>
2023-12-12 18:13:27 +0100

    clean up dispatcher

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -2,19 +2,52 @@ defmodule Dispatcher do
   use Matcher
   define_accept_types []
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
-  #
-  # docker-compose stop; docker-compose rm; docker-compose up
-  # after altering this file.
-  #
-  # match "/themes/*path" do
-  #   forward conn, path, "http://resource/themes/"
-  # end
+  #################################################################
+  # Login logic
+  #################################################################
+  match "/mock/sessions/*path" do
+    forward conn, path, "http://mocklogin/sessions/"
+  end
+  match "/sessions/*path" do
+    forward conn, path, "http://login/sessions/"
+  end
+  match "/gebruikers/*path" do
+    forward conn, path, "http://cache/gebruikers/"
+  end
+  match "/accounts/*path" do
+    forward conn, path, "http://cache/accounts/"
+  end
 
+  #################################################################
+  # Agent resources
+  #################################################################
+  match "/agents-in-position/*path" do
+    forward conn, path, "http://cache/agents-in-position/"
+  end
+  match "/mandatarissen/*path" do
+    forward conn, path, "http://cache/mandatarissen/"
+  end
+
+  #################################################################
+  # Post resources
+  #################################################################
+  match "/posts/*path" do
+    forward conn, path, "http://cache/posts/"
+  end
+  match "/mandaten/*path" do
+    forward conn, path, "http://cache/mandaten/"
+  end
+
+  #################################################################
+  # Organisation resources
+  #################################################################
   match "/bestuurseenheden/*path" do
     forward conn, path, "http://cache/bestuurseenheden/"
   end
+
+  #################################################################
+  # Besluit resources
+  #################################################################
   match "/werkingsgebieden/*path" do
     forward conn, path, "http://cache/werkingsgebieden/"
   end
@@ -27,6 +60,10 @@ defmodule Dispatcher do
   match "/bestuursorgaan-classificatie-codes/*path" do
     forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
   end
+
+  #################################################################
+  # Mandaat resources
+  #################################################################
   match "/fracties/*path" do
     forward conn, path, "http://cache/fracties/"
   end
@@ -45,17 +82,8 @@ defmodule Dispatcher do
   match "/lidmaatschappen/*path" do
     forward conn, path, "http://cache/lidmaatschappen/"
   end
-  match "/mandaten/*path" do
-    forward conn, path, "http://cache/mandaten/"
-  end
   match "/bestuursfunctie-codes/*path" do
     forward conn, path, "http://cache/bestuursfunctie-codes/"
-  end
-  delete "/mandatarissen/:id" do
-    forward conn, [], "http://mandataris-archive/" <> id <> "/archive"
-  end
-  match "/mandatarissen/*path" do
-    forward conn, path, "http://cache/mandatarissen/"
   end
   match "/mandataris-status-codes/*path" do
     forward conn, path, "http://cache/mandataris-status-codes/"
@@ -69,35 +97,81 @@ defmodule Dispatcher do
   match "/geslacht-codes/*path" do
     forward conn, path, "http://cache/geslacht-codes/"
   end
-
   match "/nationalities/*path" do
     Proxy.forward conn, path, "http://cache/nationalities/"
   end
-
   match "/identificatoren/*path" do
     forward conn, path, "http://cache/identificatoren/"
   end
-
   match "/tijdsintervallen/*path" do
     forward conn, path, "http://cache/tijdsintervallen/"
   end
 
-  match "/mock/sessions/*path" do
-    forward conn, path, "http://mocklogin/sessions/"
+  #################################################################
+  # Contact resources
+  #################################################################
+  match "/contact-punten/*path" do
+    forward conn, path, "http://cache/contact-punten/"
   end
-  match "/sessions/*path" do
-    forward conn, path, "http://login/sessions/"
-  end
-  match "/gebruikers/*path" do
-    forward conn, path, "http://cache/gebruikers/"
-  end
-  match "/accounts/*path" do
-    forward conn, path, "http://cache/accounts/"
+  match "/adressen/*path" do
+    forward conn, path, "http://cache/adressen/"
   end
 
-  match "/document-statuses/*path" do
-    forward conn, path, "http://cache/document-statuses/"
+  #################################################################
+  # Leidinggevenden resources
+  #################################################################
+  match "/bestuursfuncties/*path" do
+    forward conn, path, "http://cache/bestuursfuncties/"
   end
+  match "/functionarissen/*path" do
+    forward conn, path, "http://cache/functionarissen/"
+  end
+  match "/functionaris-status-codes/*path" do
+    forward conn, path, "http://cache/functionaris-status-codes/"
+  end
+
+  #################################################################
+  # Worship resources
+  #################################################################
+  match "/roles/*path" do
+    forward conn, path, "http://cache/roles/"
+  end
+  match "/organizations/*path" do
+    forward conn, path, "http://cache/organizations/"
+  end
+  match "/sites/*path" do
+    forward conn, path, "http://cache/sites/"
+  end
+  match "/organization-status-codes/*path" do
+    forward conn, path, "http://cache/organization-status-codes/"
+  end
+  match "/site-types/*path" do
+    forward conn, path, "http://cache/site-types/"
+  end
+  match "/participations/*path" do
+    forward conn, path, "http://cache/participations/"
+  end
+
+  #################################################################
+  # Concept scheme resources
+  #################################################################
+  get "/concept-schemes/*path" do
+    forward conn, path, "http://cache/concept-schemes/"
+  end
+  get "/concepts/*path" do
+    forward conn, path, "http://cache/concepts/"
+  end
+
+  #################################################################
+  # Random resources
+  #################################################################
+  match "/vendors/*path" do
+    forward conn, path, "http://cache/vendors/"
+  end
+
+  #################################################################
+  # File logic + resources
+  #################################################################
   get "/files/:id/download" do
     forward conn, [], "http://file/files/" <> id <> "/download"
   end
@@ -121,107 +195,23 @@ defmodule Dispatcher do
   match "/file-addresses/*path" do
     forward conn, path, "http://resource/file-addresses/"
   end
-  match "/file-address-cache-statuses/*path" do
-    forward conn, path, "http://resource/file-address-cache-statuses/"
-  end
-  ###############################################################
-  # dynamic-forms-domain.lisp
-  ###############################################################
-  match "/form-nodes/*path" do
-    forward conn, path, "http://cache/form-nodes/"
-  end
-  match "/form-inputs/*path" do
-    forward conn, path, "http://cache/form-inputs/"
-  end
-  match "/dynamic-subforms/*path" do
-    forward conn, path, "http://cache/dynamic-subforms/"
-  end
-
-  match "/input-states/*path" do
-    forward conn, path, "http://cache/input-states/"
-  end
 
   #################################################################
-  # slave leidinggevenden
+  # Extra services
   #################################################################
-  match "/bestuursfuncties/*path" do
-    forward conn, path, "http://cache/bestuursfuncties/"
+  delete "/mandatarissen/:id" do
+    forward conn, [], "http://mandataris-archive/" <> id <> "/archive"
   end
-
-  match "/functionarissen/*path" do
-    forward conn, path, "http://cache/functionarissen/"
-  end
-
-  match "/contact-punten/*path" do
-    forward conn, path, "http://cache/contact-punten/"
-  end
-
-  match "/adressen/*path" do
-    forward conn, path, "http://cache/adressen/"
-  end
-
-  match "/functionaris-status-codes/*path" do
-    forward conn, path, "http://cache/functionaris-status-codes/"
-  end
-
-  #################################################################
-  # Toezicht / supervision
-  #################################################################
-
-  match "/vendors/*path" do
-    forward conn, path, "http://cache/vendors/"
-  end
-
-  get "/concept-schemes/*path" do
-    forward conn, path, "http://cache/concept-schemes/"
-  end
-
-  get "/concepts/*path" do
-    forward conn, path, "http://cache/concepts/"
-  end
-
-  #################################################################
-  # RRN SERVICE: person-uri-for-social-security-number-service
-  #################################################################
   match "/rrn/*path" do
     forward conn, path, "http://person-uri-for-social-security-number/"
   end
-
-  #################################################################
-  # Bedienarenbeheer
-  #################################################################
-  match "/agents-in-position/*path" do
-    forward conn, path, "http://cache/agents-in-position/"
-  end
-
-  match "/posts/*path" do
-    forward conn, path, "http://cache/posts/"
-  end
-
-  match "/roles/*path" do
-    forward conn, path, "http://cache/roles/"
-  end
-
-  match "/organizations/*path" do
-    forward conn, path, "http://cache/organizations/"
-  end
-
-  match "/sites/*path" do
-    forward conn, path, "http://cache/sites/"
-  end
-
-  match "/organization-status-codes/*path" do
-    forward conn, path, "http://cache/organization-status-codes/"
-  end
-
-  match "/site-types/*path" do
-    forward conn, path, "http://cache/site-types/"
-  end
-
   match "/form-content/*path" do
     forward conn, path, "http://form-content/"
   end
 
+  #################################################################
+  # Error handling
+  #################################################################
   match "/*_" do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end

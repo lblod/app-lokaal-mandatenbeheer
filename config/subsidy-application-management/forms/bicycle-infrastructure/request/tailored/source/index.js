9a1e14a3d04c2ad95fae9d329389e4d59e182f53
Ben Fidlers <ben.fidlers@qmino.com>
2023-12-12 15:37:50 +0100

    remove subsidy-application-management config

--- config/subsidy-application-management/forms/bicycle-infrastructure/request/tailored/source/index.js
+++
@@ -1,8 +0,0 @@
-const bikeSubsidyRequestExtractor = require('./extractors/bike-subsidy-request-extractor');
-
-/**
- * NOTE: order of execution bound to position in the array
- */
-module.exports = [
-    bikeSubsidyRequestExtractor
-]

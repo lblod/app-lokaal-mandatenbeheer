2301542653cb5c076b7dda7ec424e1c0a57a6b4f
Ruben Bottu <ruben.bottu@qmino.com>
2024-03-12 09:54:38 +0100

    Added form and form extension to auth config + formatting

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -8,13 +8,14 @@ alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 
 defmodule Acl.UserGroups.Config do
-  defp access_by_role( group_string ) do
+  defp access_by_role(group_string) do
     %AccessByQuery{
-      vars: ["session_group","session_role"],
-      query: sparql_query_for_access_role( group_string ) }
+      vars: ["session_group", "session_role"],
+      query: sparql_query_for_access_role(group_string)
+    }
   end
 
-  defp sparql_query_for_access_role( group_string ) do
+  defp sparql_query_for_access_role(group_string) do
     "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     SELECT DISTINCT ?session_group ?session_role WHERE {
@@ -35,7 +36,7 @@ defmodule Acl.UserGroups.Config do
           <SESSION_ID> ext:sessionGroup/mu:uuid ?session_group;
                        ext:sessionRole ?session_role.
         }"
-      }
+    }
   end
 
   def user_groups do
@@ -46,70 +47,87 @@ defmodule Acl.UserGroups.Config do
     # many ways.  The useage of a GroupSpec and GraphCleanup are
     # common.
     [
-    # // PUBLIC
-    %GroupSpec{
-      name: "public",
-      useage: [:read],
-      access: %AlwaysAccessible{}, # This is needed otherwise you can't select a bestuurseenheid to login to.
-      graphs: [ %GraphSpec{
-                  graph: "http://mu.semte.ch/graphs/public",
-                  constraint: %ResourceConstraint{
-                    resource_types: [
-                        "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid"
-                      ] } } ] },
-
+      # // PUBLIC
+      %GroupSpec{
+        name: "public",
+        useage: [:read],
+        # This is needed otherwise you can't select a bestuurseenheid to login to.
+        access: %AlwaysAccessible{},
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/public",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid"
+              ]
+            }
+          }
+        ]
+      },
       %GroupSpec{
         name: "auth",
         useage: [:read],
         access: is_authenticated(),
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/public",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://mu.semte.ch/vocabularies/ext/FileAddress",
-                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
-                        "http://www.w3.org/ns/prov#Location",
-                        "http://mu.semte.ch/vocabularies/ext/BestuurseenheidClassificatieCode",
-                        "http://mu.semte.ch/vocabularies/ext/BestuursorgaanClassificatieCode",
-                        "http://mu.semte.ch/vocabularies/ext/Fractietype",
-                        "http://mu.semte.ch/vocabularies/ext/BestuursfunctieCode",
-                        "http://mu.semte.ch/vocabularies/ext/MandatarisStatusCode",
-                        "http://mu.semte.ch/vocabularies/ext/BeleidsdomeinCode",
-                        "http://mu.semte.ch/vocabularies/ext/GeslachtCode",
-                        "http://publications.europa.eu/ontology/euvoc#Country",
-                        "http://data.europa.eu/eli/ontology#LegalResource",
-                        "http://data.vlaanderen.be/ns/mandaat#RechtsgrondAanstelling",
-                        "http://data.vlaanderen.be/ns/mandaat#RechtsgrondBeeindiging",
-                        "http://data.vlaanderen.be/ns/mandaat#RechtstreekseVerkiezing",
-                        "http://data.vlaanderen.be/ns/mandaat#Verkiezingsresultaat",
-                        "http://mu.semte.ch/vocabularies/ext/VerkiezingsresultaatGevolgCode",
-                        "http://www.w3.org/ns/org#Role",
-                        "http://data.lblod.info/vocabularies/leidinggevenden/FunctionarisStatusCode",
-                        "http://data.lblod.info/vocabularies/leidinggevenden/Bestuursfunctie",
-                        "http://www.w3.org/2004/02/skos/core#ConceptScheme",
-                        "http://www.w3.org/2004/02/skos/core#Concept",
-                        "http://data.europa.eu/m8g/PeriodOfTime",
-                        "http://xmlns.com/foaf/0.1/Document"
-                      ] } },
-                  %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/sessions",
-                    constraint: %ResourceFormatConstraint{
-                      resource_prefix: "http://mu.semte.ch/sessions/"
-                    } } ] },
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/public",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://mu.semte.ch/vocabularies/ext/FileAddress",
+                "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+                "http://www.w3.org/ns/prov#Location",
+                "http://mu.semte.ch/vocabularies/ext/BestuurseenheidClassificatieCode",
+                "http://mu.semte.ch/vocabularies/ext/BestuursorgaanClassificatieCode",
+                "http://mu.semte.ch/vocabularies/ext/Fractietype",
+                "http://mu.semte.ch/vocabularies/ext/BestuursfunctieCode",
+                "http://mu.semte.ch/vocabularies/ext/MandatarisStatusCode",
+                "http://mu.semte.ch/vocabularies/ext/BeleidsdomeinCode",
+                "http://mu.semte.ch/vocabularies/ext/GeslachtCode",
+                "http://publications.europa.eu/ontology/euvoc#Country",
+                "http://data.europa.eu/eli/ontology#LegalResource",
+                "http://data.vlaanderen.be/ns/mandaat#RechtsgrondAanstelling",
+                "http://data.vlaanderen.be/ns/mandaat#RechtsgrondBeeindiging",
+                "http://data.vlaanderen.be/ns/mandaat#RechtstreekseVerkiezing",
+                "http://data.vlaanderen.be/ns/mandaat#Verkiezingsresultaat",
+                "http://mu.semte.ch/vocabularies/ext/VerkiezingsresultaatGevolgCode",
+                "http://www.w3.org/ns/org#Role",
+                "http://data.lblod.info/vocabularies/leidinggevenden/FunctionarisStatusCode",
+                "http://data.lblod.info/vocabularies/leidinggevenden/Bestuursfunctie",
+                "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+                "http://www.w3.org/2004/02/skos/core#Concept",
+                "http://data.europa.eu/m8g/PeriodOfTime",
+                "http://xmlns.com/foaf/0.1/Document"
+              ]
+            }
+          },
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/sessions",
+            constraint: %ResourceFormatConstraint{
+              resource_prefix: "http://mu.semte.ch/sessions/"
+            }
+          }
+        ]
+      },
       %GroupSpec{
         name: "public-r",
         useage: [:read],
         access: is_authenticated(),
-        graphs: [%GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/authenticated/public",
-                    constraint: %ResourceConstraint{
-                       resource_types: [
-                         "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
-                       ],
-                       predicates: %NoPredicates{
-                         except: [
-                           "http://mu.semte.ch/vocabularies/ext/viewOnlyModules"
-                         ] } } } ] },
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/authenticated/public",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid"
+              ],
+              predicates: %NoPredicates{
+                except: [
+                  "http://mu.semte.ch/vocabularies/ext/viewOnlyModules"
+                ]
+              }
+            }
+          }
+        ]
+      },
       %GroupSpec{
         name: "org",
         useage: [:read],
@@ -119,82 +137,115 @@ defmodule Acl.UserGroups.Config do
                   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
                   SELECT ?session_group ?session_role WHERE {
                     <SESSION_ID> ext:sessionGroup/mu:uuid ?session_group.
-                    }" },
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/organizations/",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://xmlns.com/foaf/0.1/Person",
-                        "http://xmlns.com/foaf/0.1/OnlineAccount",
-                        "http://www.w3.org/ns/adms#Identifier",
-                      ] } } ] },
+                    }"
+        },
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/organizations/",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://xmlns.com/foaf/0.1/Person",
+                "http://xmlns.com/foaf/0.1/OnlineAccount",
+                "http://www.w3.org/ns/adms#Identifier"
+              ]
+            }
+          }
+        ]
+      },
 
       # // ORG-MANDATENBEHEER
       %GroupSpec{
         name: "o-mdb-r",
         useage: [:read],
-        access: access_by_role( "LoketLB-mandaatGebruiker" ),
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/organizations/",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
-                        "http://data.vlaanderen.be/ns/mandaat#Fractie",
-                        "http://data.vlaanderen.be/ns/persoon#Geboorte",
-                        "http://www.w3.org/ns/org#Membership",
-                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
-                        "http://data.vlaanderen.be/ns/mandaat#Mandataris",
-                        "http://data.vlaanderen.be/ns/mandaat#Mandaat",
-                        "http://www.w3.org/ns/org#Post",
-                        "http://www.w3.org/ns/person#Person",
-                        "http://www.w3.org/ns/adms#Identifier",
-                        "http://purl.org/dc/terms/PeriodOfTime" ] } } ] },
+        access: access_by_role("LoketLB-mandaatGebruiker"),
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/organizations/",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
+                "http://data.vlaanderen.be/ns/mandaat#Fractie",
+                "http://data.vlaanderen.be/ns/persoon#Geboorte",
+                "http://www.w3.org/ns/org#Membership",
+                "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+                "http://data.vlaanderen.be/ns/mandaat#Mandataris",
+                "http://data.vlaanderen.be/ns/mandaat#Mandaat",
+                "http://www.w3.org/ns/org#Post",
+                "http://www.w3.org/ns/person#Person",
+                "http://www.w3.org/ns/adms#Identifier",
+                "http://purl.org/dc/terms/PeriodOfTime",
+                "http://lblod.data.gift/vocabularies/forms/Form",
+                "http://lblod.data.gift/vocabularies/forms/Extension"
+              ]
+            }
+          }
+        ]
+      },
       %GroupSpec{
         name: "o-mdb-wf",
         useage: [:write, :read_for_write],
-        access: access_by_role( "LoketLB-mandaatGebruiker" ),
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/organizations/",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
-                        "http://data.vlaanderen.be/ns/mandaat#Fractie",
-                        "http://data.vlaanderen.be/ns/persoon#Geboorte",
-                        "http://www.w3.org/ns/org#Membership",
-                        "http://data.vlaanderen.be/ns/mandaat#Mandataris",
-                        "http://data.vlaanderen.be/ns/mandaat#Mandaat",
-                        "http://www.w3.org/ns/org#Post",
-                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
-                        "http://www.w3.org/ns/person#Person",
-                        "http://www.w3.org/ns/adms#Identifier",
-                        "http://www.w3.org/ns/activitystreams#Tombstone",
-                        "http://purl.org/dc/terms/PeriodOfTime" ] } } ] },
+        access: access_by_role("LoketLB-mandaatGebruiker"),
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/organizations/",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
+                "http://data.vlaanderen.be/ns/mandaat#Fractie",
+                "http://data.vlaanderen.be/ns/persoon#Geboorte",
+                "http://www.w3.org/ns/org#Membership",
+                "http://data.vlaanderen.be/ns/mandaat#Mandataris",
+                "http://data.vlaanderen.be/ns/mandaat#Mandaat",
+                "http://www.w3.org/ns/org#Post",
+                "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+                "http://www.w3.org/ns/person#Person",
+                "http://www.w3.org/ns/adms#Identifier",
+                "http://www.w3.org/ns/activitystreams#Tombstone",
+                "http://purl.org/dc/terms/PeriodOfTime",
+                "http://lblod.data.gift/vocabularies/forms/Form",
+                "http://lblod.data.gift/vocabularies/forms/Extension"
+              ]
+            }
+          }
+        ]
+      },
 
       # // LEIDINGGEVENDEN
       %GroupSpec{
         name: "o-leidinggevende-rwf",
         useage: [:read, :write, :read_for_write],
-        access: access_by_role( "LoketLB-leidinggevendenGebruiker" ),
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/organizations/",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
-                        "http://schema.org/ContactPoint",
-                        "http://www.w3.org/ns/locn#Address",
-                        "http://data.lblod.info/vocabularies/leidinggevenden/Functionaris",
-                        "http://data.vlaanderen.be/ns/persoon#Geboorte",
-                        "http://www.w3.org/ns/person#Person",
-                        "http://www.w3.org/ns/activitystreams#Tombstone",
-                        "http://www.w3.org/ns/adms#Identifier"
-                      ] } },
-                  %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/organizations/",
-                    constraint: %ResourceConstraint{
-                      resource_types: [ "http://data.lblod.info/vocabularies/leidinggevenden/Bestuursfunctie" ],
-                      predicates: %NoPredicates{
-                        except: [
-                          "http://schema.org/contactPoint" ] } } } ] },
+        access: access_by_role("LoketLB-leidinggevendenGebruiker"),
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/organizations/",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.lblod.info/vocabularies/contacthub/AgentInPositie",
+                "http://schema.org/ContactPoint",
+                "http://www.w3.org/ns/locn#Address",
+                "http://data.lblod.info/vocabularies/leidinggevenden/Functionaris",
+                "http://data.vlaanderen.be/ns/persoon#Geboorte",
+                "http://www.w3.org/ns/person#Person",
+                "http://www.w3.org/ns/activitystreams#Tombstone",
+                "http://www.w3.org/ns/adms#Identifier"
+              ]
+            }
+          },
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/organizations/",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://data.lblod.info/vocabularies/leidinggevenden/Bestuursfunctie"
+              ],
+              predicates: %NoPredicates{
+                except: [
+                  "http://schema.org/contactPoint"
+                ]
+              }
+            }
+          }
+        ]
+      },
 
       # // CLEANUP
       #

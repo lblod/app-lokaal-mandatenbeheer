f3cfedf54478ab2d644e020221e6988eb3fdd529
Ruben Bottu <ruben.bottu@qmino.com>
2024-03-12 09:54:38 +0100

    Added form and form extension to auth config + formatting

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,20 +1,20 @@
 defmodule Dispatcher do
   use Matcher
 
-  define_accept_types [
-    json: [ "application/json", "application/vnd.api+json" ],
-    html: [ "text/html", "application/xhtml+html" ],
-    sparql: [ "application/sparql-results+json" ],
-    any: [ "*/*" ]
-  ]
+  define_accept_types(
+    json: ["application/json", "application/vnd.api+json"],
+    html: ["text/html", "application/xhtml+html"],
+    sparql: ["application/sparql-results+json"],
+    any: ["*/*"]
+  )
 
-  define_layers [ :static, :sparql, :api_services, :frontend_fallback, :resources, :not_found ]
+  define_layers([:static, :sparql, :api_services, :frontend_fallback, :resources, :not_found])
 
   options "/*path", _ do
     conn
-    |> Plug.Conn.put_resp_header( "access-control-allow-headers", "content-type,accept" )
-    |> Plug.Conn.put_resp_header( "access-control-allow-methods", "*" )
-    |> send_resp( 200, "{ \"message\": \"ok\" }" )
+    |> Plug.Conn.put_resp_header("access-control-allow-headers", "content-type,accept")
+    |> Plug.Conn.put_resp_header("access-control-allow-methods", "*")
+    |> send_resp(200, "{ \"message\": \"ok\" }")
   end
 
   ###############
@@ -22,16 +22,16 @@ defmodule Dispatcher do
   ###############
 
   # self-service
-  match "/index.html", %{ layer: :static } do
-    forward conn, [], "http://frontend/index.html"
+  match "/index.html", %{layer: :static} do
+    forward(conn, [], "http://frontend/index.html")
   end
 
-  get "/assets/*path",  %{ layer: :static } do
-    forward conn, path, "http://frontend/assets/"
+  get "/assets/*path", %{layer: :static} do
+    forward(conn, path, "http://frontend/assets/")
   end
 
-  get "/@appuniversum/*path", %{ layer: :static } do
-    forward conn, path, "http://frontend/@appuniversum/"
+  get "/@appuniversum/*path", %{layer: :static} do
+    forward(conn, path, "http://frontend/@appuniversum/")
   end
 
   #################
@@ -39,9 +39,9 @@ defmodule Dispatcher do
   #################
 
   # self-service
-  match "/*path", %{ layer: :frontend_fallback, accept: %{ html: true } } do
+  match "/*path", %{layer: :frontend_fallback, accept: %{html: true}} do
     # we don't forward the path, because the app should take care of this in the browser.
-    forward conn, [], "http://frontend/index.html"
+    forward(conn, [], "http://frontend/index.html")
   end
 
   # match "/favicon.ico", @any do
@@ -52,177 +52,215 @@ defmodule Dispatcher do
   # RESOURCES
   ###############
 
-  match "/remote-data-objects/*path", %{ layer: :resources, accept: %{ json: true } } do
-    Proxy.forward conn, path, "http://resource/remote-data-objects/"
+  match "/remote-data-objects/*path", %{layer: :resources, accept: %{json: true}} do
+    Proxy.forward(conn, path, "http://resource/remote-data-objects/")
   end
 
   #################################################################
   # Abstract resources
   #################################################################
-  match "/agents-in-position/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/agents-in-position/"
+  match "/agents-in-position/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/agents-in-position/")
   end
-  match "/posts/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/posts/"
+
+  match "/posts/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/posts/")
   end
-  match "/roles/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/roles/"
+
+  match "/roles/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/roles/")
   end
 
   #################################################################
   # Besluit resources
   #################################################################
-  match "/bestuurseenheden/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuurseenheden/"
+  match "/bestuurseenheden/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuurseenheden/")
   end
-  match "/werkingsgebieden/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/werkingsgebieden/"
+
+  match "/werkingsgebieden/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/werkingsgebieden/")
   end
-  match "/bestuurseenheid-classificatie-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
+
+  match "/bestuurseenheid-classificatie-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuurseenheid-classificatie-codes/")
   end
-  match "/bestuursorganen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuursorganen/"
+
+  match "/bestuursorganen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuursorganen/")
   end
-  match "/bestuursorgaan-classificatie-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
+
+  match "/bestuursorgaan-classificatie-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuursorgaan-classificatie-codes/")
   end
 
   #################################################################
   # Mandaat resources
   #################################################################
-  match "/fracties/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/fracties/"
+  match "/fracties/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/fracties/")
   end
-  match "/fractietypes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/fractietypes/"
+
+  match "/fractietypes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/fractietypes/")
   end
-  match "/geboortes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/geboortes/"
+
+  match "/geboortes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/geboortes/")
   end
-  match "/lidmaatschappen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/lidmaatschappen/"
+
+  match "/lidmaatschappen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/lidmaatschappen/")
   end
-  match "/mandaten/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/mandaten/"
+
+  match "/mandaten/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/mandaten/")
   end
-  match "/bestuursfunctie-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuursfunctie-codes/"
+
+  match "/bestuursfunctie-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuursfunctie-codes/")
   end
-  match "/mandatarissen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/mandatarissen/"
+
+  match "/mandatarissen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/mandatarissen/")
   end
-  match "/mandataris-status-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/mandataris-status-codes/"
+
+  match "/mandataris-status-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/mandataris-status-codes/")
   end
-  match "/beleidsdomein-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/beleidsdomein-codes/"
+
+  match "/beleidsdomein-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/beleidsdomein-codes/")
   end
-  match "/personen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/personen/"
+
+  match "/personen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/personen/")
   end
-  match "/geslacht-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/geslacht-codes/"
+
+  match "/geslacht-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/geslacht-codes/")
   end
-  match "/nationalities/*path", %{ layer: :resources, accept: %{ json: true } } do
-    Proxy.forward conn, path, "http://cache/nationalities/"
+
+  match "/nationalities/*path", %{layer: :resources, accept: %{json: true}} do
+    Proxy.forward(conn, path, "http://cache/nationalities/")
   end
-  match "/identificatoren/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/identificatoren/"
+
+  match "/identificatoren/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/identificatoren/")
   end
-  match "/tijdsintervallen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/tijdsintervallen/"
+
+  match "/tijdsintervallen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/tijdsintervallen/")
   end
 
   #################################################################
   # Contact resources
   #################################################################
-  match "/contact-punten/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/contact-punten/"
+  match "/contact-punten/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/contact-punten/")
   end
-  match "/adressen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/adressen/"
+
+  match "/adressen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/adressen/")
   end
 
   #################################################################
   # Leidinggevenden resources
   #################################################################
-  match "/bestuursfuncties/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/bestuursfuncties/"
+  match "/bestuursfuncties/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuursfuncties/")
   end
-  match "/functionarissen/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/functionarissen/"
+
+  match "/functionarissen/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/functionarissen/")
   end
-  match "/functionaris-status-codes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/functionaris-status-codes/"
+
+  match "/functionaris-status-codes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/functionaris-status-codes/")
   end
 
   #################################################################
   # Concept scheme resources
   #################################################################
-  get "/concept-schemes/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/concept-schemes/"
+  get "/concept-schemes/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/concept-schemes/")
   end
-  get "/concepts/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://cache/concepts/"
+
+  get "/concepts/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/concepts/")
   end
 
   #################################################################
   # File logic + resources
   #################################################################
-  patch "/files/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://resource/files/"
+  patch "/files/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://resource/files/")
   end
-  post "/files/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://file/files/"
+
+  post "/files/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://file/files/")
   end
+
   # TODO: find all usage of this endpoint and replace it with `POST /files`
   # This is kept to maintain compatibility with code that uses the "old" endpoint.
-  post "/file-service/files/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://file/files/"
+  post "/file-service/files/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://file/files/")
   end
-  delete "/files/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://file/files/"
+
+  delete "/files/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://file/files/")
   end
-  match "/file-addresses/*path", %{ layer: :resources, accept: %{ json: true } } do
-    forward conn, path, "http://resource/file-addresses/"
+
+  match "/file-addresses/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://resource/file-addresses/")
   end
-  get "/files/:id/download", %{ layer: :resources, accept: %{ any: true} } do
-    forward conn, [], "http://file/files/" <> id <> "/download"
+
+  get "/files/:id/download", %{layer: :resources, accept: %{any: true}} do
+    forward(conn, [], "http://file/files/" <> id <> "/download")
   end
-  get "/files/*path", %{ layer: :resources, accept: %{ any: true} } do
-    forward conn, path, "http://resource/files/"
+
+  get "/files/*path", %{layer: :resources, accept: %{any: true}} do
+    forward(conn, path, "http://resource/files/")
   end
 
   #################################################################
   # Login logic
   #################################################################
   match "/mock/sessions/*path" do
-    forward conn, path, "http://mocklogin/sessions/"
+    forward(conn, path, "http://mocklogin/sessions/")
   end
-  match "/gebruikers/*path", %{ layer: :resources, accept: %{ any: true}}  do
-    forward conn, path, "http://cache/gebruikers/"
+
+  match "/gebruikers/*path", %{layer: :resources, accept: %{any: true}} do
+    forward(conn, path, "http://cache/gebruikers/")
   end
-  match "/accounts/*path", %{ layer: :resources, accept: %{ any: true}}  do
-    forward conn, path, "http://cache/accounts/"
+
+  match "/accounts/*path", %{layer: :resources, accept: %{any: true}} do
+    forward(conn, path, "http://cache/accounts/")
   end
-  match "/sessions/*path", %{ layer: :resources, accept: %{ any: true}} do
-    Proxy.forward conn, path, "http://login/sessions/"
+
+  match "/sessions/*path", %{layer: :resources, accept: %{any: true}} do
+    Proxy.forward(conn, path, "http://login/sessions/")
   end
 
   #################################################################
   # Extra services
   #################################################################
-  delete "/mandatarissen/:id", %{ layer: :api_services, accept: %{ any: true}}  do
-    forward conn, [], "http://mandataris-archive/" <> id <> "/archive"
+  delete "/mandatarissen/:id", %{layer: :api_services, accept: %{any: true}} do
+    forward(conn, [], "http://mandataris-archive/" <> id <> "/archive")
   end
-  match "/rrn/*path", %{ layer: :api_services, accept: %{ any: true}}  do
-    forward conn, path, "http://person-uri-for-social-security-number/"
+
+  match "/rrn/*path", %{layer: :api_services, accept: %{any: true}} do
+    forward(conn, path, "http://person-uri-for-social-security-number/")
   end
-  match "/form-content/*path", %{ layer: :api_services, accept: %{ any: true}}  do
-    forward conn, path, "http://form-content/"
+
+  match "/form-content/*path", %{layer: :api_services, accept: %{any: true}} do
+    forward(conn, path, "http://form-content/")
   end
+
   match "/adressenregister/*path" do
+    forward(conn, path, "http://adressenregister/")
+  end
+
   #################################################################
   # Forms
   #################################################################
@@ -239,14 +277,13 @@ defmodule Dispatcher do
   # LDES
   #################################################################
   get "/streams/ldes/*path" do
-    forward conn, path, "http://ldes-backend"
+    forward(conn, path, "http://ldes-backend")
   end
 
   #################
   # NOT FOUND
   #################
-  match "/*_path", %{ layer: :not_found } do
-    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  match "/*_path", %{layer: :not_found} do
+    send_resp(conn, 404, "Route not found.  See config/dispatcher.ex")
   end
-
 end

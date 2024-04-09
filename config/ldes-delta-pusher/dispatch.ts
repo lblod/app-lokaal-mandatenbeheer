e65d9983cbb774ac913f2568b3a4286af51fed85
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-09 15:40:44 +0200

    LMB-365: filter out inserts of history graph

--- config/ldes-delta-pusher/dispatch.ts
+++ config/ldes-delta-pusher/dispatch.ts
@@ -3,6 +3,21 @@ import { handleRegularTypes } from "./handle-regular-types";
 import { handleMandatarisType } from "./handle-mandataris-type";
 
 export default async function dispatch(changesets: Changeset[]) {
-  handleRegularTypes(changesets);
-  handleMandatarisType(changesets);
+  const nonHistoryChangesets = filterOutHistoryChanges(changesets);
+
+  handleRegularTypes(nonHistoryChangesets);
+  handleMandatarisType(nonHistoryChangesets);
+}
+
+function filterOutHistoryChanges(changesets: Changeset[]) {
+  const doesNotStartWithHistoryUri = (quad) => !quad.graph.value.startsWith('http://mu.semte.ch/graphs/formHistory');
+
+  return changesets.map(change => {
+    const { inserts, deletes } = change;
+
+    return {
+      inserts: inserts.filter(doesNotStartWithHistoryUri),
+      deletes: deletes.filter(doesNotStartWithHistoryUri),
+    }
+  })
 }

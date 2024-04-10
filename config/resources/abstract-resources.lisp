0c48208416d5818ac1b1ba90cb98f3a390ff3862
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-10 17:18:27 +0200

    Remove role resource

--- config/resources/abstract-resources.lisp
+++ config/resources/abstract-resources.lisp
@@ -22,20 +22,10 @@
 
 (define-resource post ()
   :class (s-prefix "org:Post")
-  :has-one `((role :via ,(s-prefix "org:role")
-                   :as "role"))
   :has-many `((agent-in-position :via ,(s-prefix "org:holds")
                                  :inverse t
                                  :as "agents-in-position"))
   :resource-base (s-url "http://data.lblod.info/id/posities/")
   :features '(include-uri)
   :on-path "posts"
-)
-
-(define-resource role ()
-  :class (s-prefix "org:Role")
-  :properties `((:label :string ,(s-prefix "skos:prefLabel")))
-  :resource-base (s-url "http://data.lblod.info/id/rollen/")
-  :features '(include-uri)
-  :on-path "roles"
 )

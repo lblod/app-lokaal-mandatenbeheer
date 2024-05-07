950ec72abd09af847bb800fb94a46cafc65673df
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-07 09:59:04 +0200

    LMB-420: add path in dispatcher to get the bestuurseenheid-contact-info

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -86,6 +86,10 @@ defmodule Dispatcher do
     forward(conn, path, "http://cache/bestuurseenheid-classificatie-codes/")
   end
 
+  match "/bestuurseenheid-contact-info/*path", %{layer: :resources, accept: %{json: true}} do
+    forward(conn, path, "http://cache/bestuurseenheid-contact-info/")
+  end
+
   match "/bestuursorganen/*path", %{layer: :resources, accept: %{json: true}} do
     forward(conn, path, "http://cache/bestuursorganen/")
   end

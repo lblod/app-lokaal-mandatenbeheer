b2f636a8cfefda9d5619a52fb107e015e9a81d05
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-09-19 09:52:46 +0200

    LMB-339: fix domain property effectief-at

--- config/resources/external-mandaat.lisp
+++ config/resources/external-mandaat.lisp
@@ -83,7 +83,7 @@
                 (:duplication-reason :string ,(s-prefix "skos:changeNote"))
                 (:link-to-besluit :string ,(s-prefix "lmb:linkToBesluit"))
                 (:modified :datetime ,(s-prefix "dct:modified"))
-                (:effectiefAt :datetime ,(s-prefix "lmb:effectiefAt")))
+                (:effectief-at :datetime ,(s-prefix "lmb:effectiefAt")))
   :has-many `((mandataris :via ,(s-prefix "mandaat:isTijdelijkVervangenDoor")
                           :as "tijdelijke-vervangingen")
               (mandataris :via ,(s-prefix "mandaat:isTijdelijkVervangenDoor")

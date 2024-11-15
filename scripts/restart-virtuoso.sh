5fc3846498001a6ce67855cf8c35e5bd0ffbc045
Ben Fidlers <ben.fidlers@qmino.com>
2024-11-15 12:24:15 +0100

    fix flag in restart-virtuoso script

--- scripts/restart-virtuoso.sh
+++ scripts/restart-virtuoso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "WARNING this script will restart virtuoso in 60 seconds!"
 sleep 60
-docker compose exec -t virtuoso isql-v <<EOF
+docker compose exec -T virtuoso isql-v <<EOF
 CHECKPOINT;
 exit;
 EOF

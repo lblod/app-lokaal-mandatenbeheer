eb4fd2e9783221000aac1970fae35ab6e9cba3bc
Ben Fidlers <ben.fidlers@qmino.com>
2024-11-15 11:57:26 +0100

    add script to restart virtuoso

---
+++ scripts/restart-virtuoso.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "WARNING this script will restart virtuoso in 60 seconds!"
+sleep 60
+docker compose exec -t virtuoso isql-v <<EOF
+CHECKPOINT;
+exit;
+EOF
+sleep 60
+
+docker compose kill virtuoso
+docker compose start

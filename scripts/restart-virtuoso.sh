95e0fc31767bf96cecf25d42d1e790e685a6bd7a
karel kremer <karel.kremer@redpencil.io>
2024-11-15 12:54:55 +0100

    change dir to script dir first and then to app dir relatively so we run the docker compose command in the right directory

--- scripts/restart-virtuoso.sh
+++ scripts/restart-virtuoso.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo "WARNING this script will restart virtuoso in 60 seconds!"
-sleep 60
+cd "$(dirname "$0")"
+cd ../
+echo "WARNING this script will restart virtuoso in 10 seconds!"
+sleep 10
 docker compose exec -T virtuoso isql-v <<EOF
 CHECKPOINT;
 exit;
 EOF
-sleep 60
+sleep 10
 
 docker compose kill virtuoso
 docker compose start

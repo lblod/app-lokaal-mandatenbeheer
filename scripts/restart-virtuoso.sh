843a955c00b01cf70440595b3c2c41188dcf4a74
Niels V <nvdk@users.noreply.github.com>
2024-11-21 15:22:56 +0100

    Update restart-virtuoso.sh
    safe shutdown of virtuoso

--- scripts/restart-virtuoso.sh
+++ scripts/restart-virtuoso.sh
@@ -4,10 +4,7 @@ cd ../
 echo "WARNING this script will restart virtuoso in 10 seconds!"
 sleep 10
 docker compose exec -T virtuoso isql-v <<EOF
-CHECKPOINT;
+exec('CHECKPOINT');
+exec('shutdown')
 exit;
 EOF
-sleep 10
-
-docker compose kill virtuoso
-docker compose start

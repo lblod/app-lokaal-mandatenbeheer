1fd9e448fc699fc55a583afe0108db8e076c2241
karel kremer <karel.kremer@redpencil.io>
2024-11-22 08:45:46 +0100

    add missing ;

--- scripts/restart-virtuoso.sh
+++ scripts/restart-virtuoso.sh
@@ -5,6 +5,6 @@ echo "WARNING this script will restart virtuoso in 10 seconds!"
 sleep 10
 docker compose exec -T virtuoso isql-v <<EOF
 exec('CHECKPOINT');
-exec('shutdown')
+exec('shutdown');
 exit;
 EOF

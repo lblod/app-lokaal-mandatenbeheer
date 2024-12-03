558fd82b9dc597e7fbe453546b5d2fa2cf29c01f
karel kremer <karel.kremer@redpencil.io>
2024-12-03 11:34:43 +0100

    Revert "Revert "use the v1.0.0 ldes-delta-pusher library without the http requests to create ldes entries""
    This reverts commit 73a24a48838beed7891622d37a15f0c6037b6417.

--- config/ldes-delta-pusher/publisher.ts
+++ config/ldes-delta-pusher/publisher.ts
@@ -1,7 +1,7 @@
 import { sparqlEscapeUri, sparqlEscape } from "mu";
 import { querySudo } from "@lblod/mu-auth-sudo";
-import { LDES_ENDPOINT } from "../config";
-import fetch from "node-fetch";
+import { addData, getConfigFromEnv } from "@lblod/ldes-producer";
+import { LDES_FRAGMENTER } from "../config";
 import { log } from "./logger";
 import {
   defaultProperties,
@@ -9,6 +9,8 @@ import {
   officialPredicates,
 } from "./ldes-instances";
 import dispatch from "./dispatch";
+
+const ldesProducerConfig = getConfigFromEnv();
 
 export type LDES_TYPE = "public" | "abb" | "internal";
 export type TypesWithFilter = {
@@ -86,28 +88,6 @@ const fetchSubjectData = async (
   return data.results.bindings.map(bindingToTriple).join("\n");
 };
 
-async function sendLDESRequest(type: string, body: string, retriesLeft = 3) {
-  log(`Sending data to LDES endpoint ${LDES_ENDPOINT}${type}`, "debug");
-  await fetch(`${LDES_ENDPOINT}${type}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/turtle",
-    },
-    // xsd prefix is used in the types of the result data, so it needs to be declared.
-    body: `@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n${body}`,
-  }).catch(async (e) => {
-    if (retriesLeft > 0) {
-      log(
-        `Error sending data to LDES endpoint ${type} (retrying): ${e}`,
-        "error"
-      );
-      sendLDESRequest(type, body, retriesLeft - 1);
-    } else {
-      log(`Error sending data to LDES endpoint ${type}: ${e}`, "error");
-    }
-  });
-}
-
 const datatypeNames = {
   "http://www.w3.org/2001/XMLSchema#dateTime": "dateTime",
   "http://www.w3.org/2001/XMLSchema#date": "date",
@@ -150,12 +130,15 @@ export const publish = async (
   } else {
     targets = Object.keys(subject.ldesType);
   }
+  const toRepublish: any[] = [];
   await Promise.all(
     targets.map(async (target) => {
       if (ldesInstances[target].entities[subject.type]?.republishRelated) {
-        await republishRelated(
-          subject,
-          ldesInstances[target].entities[subject.type].republishRelated
+        toRepublish.push(
+          ...(await fetchRelatedToRepublish(
+            subject,
+            ldesInstances[target].entities[subject.type].republishRelated
+          ))
         );
       }
 
@@ -168,17 +151,20 @@ export const publish = async (
         `[${target}] Publishing data for subject ${subject.uri}:\n${data}`,
         "debug"
       );
-      return sendLDESRequest(target, data).catch((e) => {
-        log(
-          `Error publishing data for subject ${subject.uri} to LDES endpoint ${subject.ldesType}: ${e}`,
-          "error"
-        );
+      const safeData = `@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n${data}`;
+
+      await addData(ldesProducerConfig, {
+        contentType: "text/turtle",
+        folder: target,
+        body: safeData,
+        fragmenter: LDES_FRAGMENTER,
       });
     })
   );
+  await republishRelated(toRepublish);
 };
 
-async function republishRelated(
+async function fetchRelatedToRepublish(
   subject: InterestingSubject,
   predicates: string[]
 ) {
@@ -194,15 +180,25 @@ async function republishRelated(
     }
   `);
   const fakeDeltas = relatedSubjects.results.bindings.map((subject) => ({
-    inserts: [
-      {
-        subject: { value: subject.related.value },
-        predicate: { value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" },
-        object: { value: subject.type.value },
-        graph: { value: subject.g.value },
-      },
-    ],
-    deletes: [],
+    subject: { value: subject.related.value },
+    predicate: { value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" },
+    object: { value: subject.type.value },
+    graph: { value: subject.g.value },
   }));
-  await dispatch(fakeDeltas);
+  return fakeDeltas;
+}
+
+async function republishRelated(toRepublish) {
+  if (toRepublish.length === 0) {
+    return;
+  }
+  const uniqueSubjects = {};
+  toRepublish.forEach((delta) => {
+    uniqueSubjects[delta.subject.value + delta.object.value] = delta;
+  });
+  const fakeCompleteDelta = {
+    inserts: Object.values(uniqueSubjects),
+    deletes: [],
+  };
+  await dispatch([fakeCompleteDelta]);
 }

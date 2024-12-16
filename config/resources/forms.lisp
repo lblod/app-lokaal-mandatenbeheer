0ee6565b54ee356ff64001022a30fd5086840efe
karel kremer <karel.kremer@redpencil.io>
2024-12-16 10:53:10 +0100

    create form extensions in app

--- config/resources/forms.lisp
+++ config/resources/forms.lisp
@@ -22,3 +22,57 @@
   :resource-base (s-url "http://data.lblod.info/id/lmb/forms/")
   :on-path "form-extensions"
 )
+
+(define-resource form-field ()
+  :class (s-prefix "form:Field")
+  :properties `((:name :string ,(s-prefix "sh:name"))
+                (:order :number ,(s-prefix "sh:order"))
+                ;; this does not allow for nested paths, only direct properties. See if we can work around that? e.g. for address
+                (:path :url ,(s-prefix "sh:path"))
+                ;; json encoded options :(
+                (:options :string ,(s-prefix "form:options")))
+  ;; simplification, should be part of group and then form but that will be too complicated for our users
+  :has-one `((form-extension :via ,(s-prefix "ext:partOfForm")
+                   :as "form")
+              (display-type :via ,(s-prefix "form:displayType")
+                            :as "display-type")
+              (library-entry :via ,(s-prefix "prov:wasDerivedFrom")
+                             :as "library-entry"))
+  :resource-base (s-url "http://data.lblod.info/id/lmb/form-fields/")
+  :on-path "form-fields"
+)
+
+(define-resource display-type ()
+  :class (s-prefix "ext:DisplayType")
+  :properties `((:label :string ,(s-prefix "skos:prefLabel")))
+  :resource-base (s-url "http://data.lblod.info/id/concept/DisplayType/")
+  :features '(include-uri)
+  :on-path "display-types"
+)
+
+(define-resource library-entry ()
+  :class (s-prefix "ext:FormLibraryEntry")
+  :properties `((:name :string ,(s-prefix "sh:name"))
+                (:order :number ,(s-prefix "sh:order"))
+                ;; this does not allow for nested paths, only direct properties. See if we can work around that? e.g. for address
+                (:path :url ,(s-prefix "sh:path"))
+                ;; json encoded options :(
+                (:options :string ,(s-prefix "form:options")))
+  ;; simplification, should be part of group and then form but that will be too complicated for our users
+  :has-one `((display-type :via ,(s-prefix "form:displayType")
+                            :as "display-type"))
+  :resource-base (s-url "http://data.lblod.info/id/lmb/form-library-entries/")
+  :features '(include-uri)
+  :on-path "library-entries"
+)
+
+(define-resource library ()
+  :class (s-prefix "ext:FormLibrary")
+  :properties `((:name :string ,(s-prefix "dct:title")))
+  :has-many `((library-entry :via ,(s-prefix "ext:hasLibraryEntry")
+                             :as "entries"))
+  :resource-base (s-url "http://data.lblod.info/id/lmb/form-libraries/")
+  :features '(include-uri)
+  :on-path "libraries"
+)
+

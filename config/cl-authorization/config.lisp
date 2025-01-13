853074548ac4ce51723f6500ade3af4f607f4808
karel kremer <karel.kremer@redpencil.io>
2025-01-13 12:53:23 +0100

    allow complex paths in library fields, example datum van overlijden

--- config/cl-authorization/config.lisp
+++ config/cl-authorization/config.lisp
@@ -114,6 +114,7 @@
   ("contacthub:AgentInPositie" -> _)
   ("mandaat:Fractie" -> _)
   ("persoon:Geboorte" -> _)
+  ("persoon:Overlijden" -> _)
   ("org:Membership" -> _)
   ("besluit:Besluit" -> _)
   ("besluit:Artikel" -> _)

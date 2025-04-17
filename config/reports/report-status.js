909d6918a867f7ad267900b281f3241681bda3a9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-17 12:36:19 +0200

    lmb-1531: add cron that flags report-statuses when they take up more than 30 minutes

--- config/reports/report-status.js
+++ config/reports/report-status.js
@@ -1,6 +1,48 @@
 import { sparqlEscapeUri, sparqlEscapeString, sparqlEscapeDateTime } from "mu";
 import { updateSudo } from "@lblod/mu-auth-sudo";
 import { v4 as uuidv4 } from "uuid";
+import cron from 'node-cron';
+
+// 30 minutes cron
+cron.schedule('*/30 * * * *', async () => {
+  console.log('\nFlag statuses that are expected to be crashed.');
+  const momentWhenStatusesAreCrashed = new Date();
+  const queryString = `
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+    INSERT {
+      GRAPH ?g {
+        ?status ext:flaggedAsCrashed ${sparqlEscapeDateTime(new Date())} .
+      }
+    }
+    WHERE {
+      GRAPH ?g {
+        ?status a ext:ReportStatus .
+        ?status dct:created ?startedAt .
+
+        FILTER ( ?startedAt < ${sparqlEscapeDateTime(momentWhenStatusesAreCrashed)})
+
+        FILTER NOT EXISTS {
+          ?status ext:flaggedAsCrashed ?isFlagged .
+        }
+
+        FILTER NOT EXISTS {
+          ?status dct:issued ?finishedAt .
+          ?status ext:forReport ?report .
+        }
+      }
+      ?g ext:ownedBy ?bestuurseenheid .
+    }
+  `;
+
+  try {
+    await updateSudo(queryString);
+    console.log('Done flagging report-statuses that where expected to be crashed.');
+  } catch (e) {
+    console.log('\nSomething unexpected went wrong while cleaning up the report-statuses.', e);
+  }
+});
 
 export async function insertReportStatusInGraphs(uriAndUuid, namedGraphs) {
   const uuid = uuidv4();
@@ -22,6 +64,10 @@ export async function insertReportStatusInGraphs(uriAndUuid, namedGraphs) {
         ${namedGraphs.map((g) => sparqlEscapeUri(g)).join("\n")}
       }
       ?g ext:ownedBy ?ocmwOrMunicipality .
+
+      OPTIONAL {
+      
+      }
     }
     `;
   try {
@@ -35,9 +81,13 @@ export async function insertReportStatusInGraphs(uriAndUuid, namedGraphs) {
 export async function updateReportStatusWithReport(statusUri, reportUri, namedGraphs) {
   const queryString = `
     PREFIX dct: <http://purl.org/dc/terms/>
-    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
 
+    DELETE {
+      GRAPH ?g {
+        ${sparqlEscapeUri(statusUri)} ext:flaggedAsCrashed ?isCrashedAt .
+      }
+    }
     INSERT {
       GRAPH ?g {
         ${sparqlEscapeUri(statusUri)} dct:issued ${sparqlEscapeDateTime(new Date())} .
@@ -49,6 +99,10 @@ export async function updateReportStatusWithReport(statusUri, reportUri, namedGr
         ${namedGraphs.map((g) => sparqlEscapeUri(g)).join("\n")}
       }
       ${sparqlEscapeUri(statusUri)} a ext:ReportStatus .
+
+      OPTIONAL {
+        ${sparqlEscapeUri(statusUri)} ext:flaggedAsCrashed ?isCrashedAt .
+      }
     }
     `;
   try {

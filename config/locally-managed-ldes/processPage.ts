5784fbe8b711cc5bdd822c4a76000d2b5ab2e46d
karel kremer <karel.kremer@redpencil.io>
2025-05-20 13:23:35 +0200

    take tombstones into account (sortof)

--- config/locally-managed-ldes/processPage.ts
+++ config/locally-managed-ldes/processPage.ts
@@ -26,7 +26,7 @@ async function replaceExistingData() {
   await replaceMandatees(options, env);
   await replaceMembership(options, env);
   await replacePeople(options, env);
-  // TODO add tombstones
+  await replaceTombstones(options, env);
 }
 
 async function replaceFracties(connectionOptions, env) {
@@ -52,6 +52,46 @@ async function replaceFracties(connectionOptions, env) {
         ?versionedMember a mandaat:Fractie .
         ?versionedMember ?pNew ?oNew .
         ?versionedMember org:linkedTo ?bestuurseenheid .
+        ?versionedMember ext:owningBestuurseenheid | <http://mu.semte.ch/vocabularies/ext/relatedTo> ?bestuurseenheid .
+        FILTER (?pNew NOT IN ( ${sparqlEscapeUri(
+          env.VERSION_PREDICATE
+        )}, ${sparqlEscapeUri(env.TIME_PREDICATE)} ))
+      }
+      ?targetGraph ext:ownedBy ?bestuurseenheid .
+      OPTIONAL {
+        GRAPH ?targetGraph {
+          ?s ?pOld ?oOld.
+        }
+      }
+    }`,
+    {},
+    connectionOptions
+  );
+}
+
+async function replaceTombstones(connectionOptions, env) {
+  // this is likely wishful thinking as tombstones on our own feed are often just in public, but we can't risk that. Approach should be checked stream per stream and this is good enough for now. You can get the active stream from the environment and decide on the graph that way
+  await updateSudo(
+    `
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+    PREFIX org: <http://www.w3.org/ns/org#>
+    PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    DELETE {
+      GRAPH ?targetGraph {
+        ?s ?pOld ?oOld.
+      }
+    }
+    INSERT {
+      GRAPH ?targetGraph {
+        ?s ?pNew?oNew.
+      }
+    } WHERE {
+      GRAPH ${sparqlEscapeUri(BATCH_GRAPH)} {
+        ?stream <https://w3id.org/tree#member> ?versionedMember .
+        ?versionedMember ${sparqlEscapeUri(env.VERSION_PREDICATE)} ?s .
+        ?versionedMember a <http://www.w3.org/ns/activitystreams#Tombstone> .
+        ?versionedMember ?pNew ?oNew .
         ?versionedMember ext:owningBestuurseenheid | <http://mu.semte.ch/vocabularies/ext/relatedTo> ?bestuurseenheid .
         FILTER (?pNew NOT IN ( ${sparqlEscapeUri(
           env.VERSION_PREDICATE

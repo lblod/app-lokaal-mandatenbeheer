d96dba83e3e8d9fbb25ea899240db19700f5aa01
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-11-20 12:40:13 +0100

    feat: check if the graphs ar empty before dropping all the graphs of history
    Refs: lmb-1792

--- scripts/local-deployment-db-preparation/cleanup-history-graphs.sh
+++ scripts/local-deployment-db-preparation/cleanup-history-graphs.sh
@@ -15,6 +15,27 @@ $ISQL exec="SPARQL
 totalGraphCount=$(grep -o '[0-9]*' countHistoryGraphs.txt) 
 rm -rf ./countHistoryGraphs.txt
 
+$ISQL exec="SPARQL
+  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  SELECT COUNT(DISTINCT ?g) AS ?count
+  WHERE {
+    {
+      ?g a <http://mu.semte.ch/vocabularies/ext/FormHistory> .
+    }
+    GRAPH ?g {
+      ?s a ?type .
+    }
+  }
+;" > countHistoryGraphsWithTriples.txt
+totalGraphsWithTriplesCount=$(grep -o '[0-9]*' countHistoryGraphsWithTriples.txt)
+rm -rf ./countHistoryGraphsWithTriples.txt
+
+if [ $totalGraphsWithTriplesCount -eq "0" ]; then
+  echo "All history graphs are empty"
+  exit 0;
+fi
+
+
 batchSize=100
 totalBatches=$(( (totalGraphCount + batchSize - 1) / batchSize ))
 echo "Total graphs found: $totalGraphCount"

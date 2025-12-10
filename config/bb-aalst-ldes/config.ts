2dbe93dab2b1b6514ee56ee73eea70b5928b2197
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-12-10 11:40:30 +0100

    fix: mandataris not beeing added because of https
    Refs: lmb-1813

--- config/bb-aalst-ldes/config.ts
+++ config/bb-aalst-ldes/config.ts
@@ -1,24 +1,14 @@
-import {
-  LDES_BASE,
-  FIRST_PAGE,
-  TARGET_GRAPH,
-  STATUS_GRAPH,
-  EXTRA_HEADERS,
-  VERSION_PREDICATE,
-  TIME_PREDICATE,
-} from "../environment";
-
 export default {
   endpoints: [
     {
       name: "Public Stream",
-      LDES_BASE,
-      FIRST_PAGE,
-      TARGET_GRAPH,
-      STATUS_GRAPH,
-      EXTRA_HEADERS,
-      VERSION_PREDICATE,
-      TIME_PREDICATE,
+      LDES_BASE: process.env.LDES_BASE,
+      FIRST_PAGE: process.env.FIRST_PAGE,
+      TARGET_GRAPH: process.env.TARGET_GRAPH,
+      STATUS_GRAPH: process.env.STATUS_GRAPH,
+      EXTRA_HEADERS: process.env.EXTRA_HEADERS,
+      VERSION_PREDICATE: process.env.VERSION_PREDICATE,
+      TIME_PREDICATE: process.env.TIME_PREDICATE,
     },
   ],
 };
